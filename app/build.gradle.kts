cd3581a9e7492f0d57adc06ee7e2a286f7dd49e5
Rae <siaray88@gmail.com>
2025-11-12 16:46:30 +0800

    Added dependencies, strings.xml, colors.xml and some assets from old version. Next step should be integrating fragments and looking into VM for most, but first themes.

---
+++ app/build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("androidx.navigation.safeargs.kotlin")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.reborn.wasteless"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.reborn.wasteless"
+        minSdk = 25
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    //glide dependencies (img loading & cache)
+    implementation(libs.glide)
+    annotationProcessor(libs.compiler)
+
+    //noinspection UseTomlInstead
+    implementation("com.github.dhaval2404:imagepicker:2.1")
+
+    //firebase dependencies
+    implementation(platform(libs.firebase.bom))
+    implementation (libs.firebase.storage)
+    implementation (libs.firebase.firestore)
+    implementation (libs.firebase.auth)
+    implementation(libs.firebase.analytics)
+
+    //androidx splashscreen
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
